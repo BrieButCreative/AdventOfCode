d0b285d7411ccf616925aadf1a2774df63fbf383
Brie <Pr0fessionelle_Mail@gmx.de>
2025-08-05 18:48:25 +0200

    Getting ready, kinda depressed and tired rn

---
+++ 2024/DayThree/puta'.py
@@ -0,0 +1,15 @@
+def mult(file: str) -> int:
+    '''
+    Reads out a given file and multiplies x and y
+    for each 'mul(x,y)' in the file.
+    If there is any character obstructing the
+    'mul' command it gets ignored.
+    The numbers get added up which is the return-value.
+
+    >>> mult("testInput.txt")
+    161
+    '''
+
+
+if __name__ == "__main__":
+    print(mult("testInput"))
